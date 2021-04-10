feb4fb8be9dbf562712e81c11f45fce367157690
Martin Cody <martinfcody@gmail.com>
2021-04-10 20:05:38 +0100

    Test -Most recent film

--- main.py
+++ main.py
@@ -4,22 +4,29 @@ import numpy as np
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def printMostRecentMovie(df):
+    #df_sorted = df.sort_values('release_date', ascending=False)
+    df.sort_values('release_date', ascending=False, inplace=True)
+    print(
+        'Original title: ' +
+        df['original_title'].iloc[0] +
+        ', release date' +
+        df['release_date'].iloc[0]
+    )
 
-# Press the green button in the gutter to run the script.
-#
+
 print_hi('PyCharm')
 
 print_hi('Martin')
 
-file_url = 'https://raw.githubusercontent.com/Martmedia/UCDPA_MartinCody/main/tmdb_movies_data.csv'
+#file_url = 'https://raw.githubusercontent.com/Martmedia/UCDPA_MartinCody/main/tmdb_movies_data.csv'
+file_url = 'tmdb_movies_data.csv'
 martmovies = pd.read_csv(file_url)
 
 for x in range(0, 5):
     # print("Genre: " + dataFrame.loc[x, 'genres'] + ", budget: " + str(dataFrame.loc[x, 'budget']))
     print("Genre: " + martmovies.loc[x, 'genres'])
-martmovies.info()
-
-
+#martmovies.info()
+printMostRecentMovie(martmovies)
