0f127da3873c73f13917656b252c879077bf33d0
Martin Cody <martinfcody@gmail.com>
2021-04-21 22:25:48 +0100

    backup

--- main.py
+++ main.py
@@ -1,49 +1,101 @@
 #Import required packages
+import datetime
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
-from shapely.geometry import Point
-import geo as gpd
+import bokeh as bp
 
+#Functions (Reusable Code)
 def printMostRecentMovie(df):
-    df_sorted = df.sort_values('release_date', ascending=False)
     df.sort_values('release_date', ascending=False, inplace=True)
-    print(
-        'Original title: ' +
-        df['original_title'].iloc[0] +
-        ', release date' +
-        df['release_date'].iloc[0]
-    )
+    print("Most recent film in dataset is: " + df['original_title'].iloc[0] + ", released on " + str(df['release_date'].iloc[0].date()) )
+    print("Least recent film in dataset is: " + df['original_title'].iloc[-1] + ", released on " + str(df['release_date'].iloc[-1].date()) )
+    return
+
+def printMostProfitableMovie(df):
+    df.sort_values('profitability', ascending=False, inplace=True)
+    print("Most profitable film in dataset is: " + df['original_title'].iloc[0] + ", which had profitability of "
+          + str(df['profitability'].iloc[0] )  + ", budget of " +str(df['budget'].iloc[0]) + ", revenue of " + str(df['revenue'].iloc[0]))
+    print("Least profitable film in dataset is: " + df['original_title'].iloc[-1] + ", which had profitability of "
+          + str(df['profitability'].iloc[-1] ) + ", budget of " + str(df['budget'].iloc[-1]) + ", revenue of " + str(df['revenue'].iloc[-1]))
+    return
+
+def removeLowBudgetFilms(df):
+    df.drop(df[ df.budget < 500000 ].index, inplace=True)
+    return
+
+def removeDocumentaries(df):
+    df.drop(df[df.first_genre == "Documentary"].index, inplace=True)
+    return
+
+def removeLowRevenueFilms(df):
+    df.drop(df[ df.revenue < 50000 ].index, inplace=True)
+    return
+
+#Remove all rows 'revenue'/'budget' where value = 0
+def removeInvalidRows(df):
+    df.drop(df[df.budget == 0].index, inplace=True)
+    df.drop(df[df.revenue == 0].index, inplace=True)
+    return
+
+def removePreJawsMovies(df):
+    df.drop(df[df.release_year < 1975].index, inplace=True)
+    return
+
+def addProfitability(df):
+    df['profitability'] = df['revenue'] / df['budget']
+    return
+
+def addFirstGenre(df):
+    df['first_genre'] = df['genres'] .apply( lambda val : val.split('|')[0])
+    return
+
+def removeUnnecessaryColumns(df):
+    df.drop(['id', 'imdb_id', 'homepage', 'tagline', 'budget_adj', 'revenue_adj', 'vote_count'], inplace=True, axis=1)
+    return
+
+def convertReleaseDate(df):
+    martmovies['release_date'] = pd.to_datetime(martmovies['release_date'], infer_datetime_format=True, errors='raise')
+    return
 
 
 file_url = 'https://raw.githubusercontent.com/Martmedia/UCDPA_MartinCody/main/tmdb_movies_data.csv'
 file_url = 'tmdb_movies_data.csv'
 martmovies = pd.read_csv(file_url)
 
+#Cleaning up of dataframe (original = 10866 rows/21cols)
 
-#for x in range(0, 5):
-    # print("Genre: " + dataFrame.loc[x, 'genres'] + ", budget: " + str(dataFrame.loc[x, 'budget']))
-    #print("Genre: " + martmovies.loc[x, 'genres'])
+#Convert 'release_date' column to uk-format date object(no datetime dtype indicated)
+convertReleaseDate(martmovies)
+
+#Remove uneccessary columns
+removeUnnecessaryColumns(martmovies)
+
+#Delete rows wher columns revenue or budget = 0
+removeInvalidRows(martmovies)
+
+
+addProfitability(martmovies)
+addFirstGenre(martmovies)
+
+removePreJawsMovies(martmovies)
+removeDocumentaries(martmovies)
+removeLowBudgetFilms(martmovies)
+removeLowRevenueFilms(martmovies)
+
+
+
+martmovies.info()
+
 
 printMostRecentMovie(martmovies)
 
-#Cleaning up of dataframe (original = 10866 rows/21cols)
+printMostProfitableMovie(martmovies)
 
-#1. Convert 'release_date' column to uk-format date object(no datetime dtype indicated)
-martmovies['release_date'] = pd.to_datetime(martmovies['release_date'])
-martmovies.info()
+sns.scatterplot(data=martmovies, x="release_date", y="popularity", hue='popularity')
 
-#2. Remove uneccessary columns
-martmovies.drop('id', inplace=True, axis=1)
-martmovies.drop('imdb_id', inplace=True, axis=1)
-martmovies.drop('homepage', inplace=True, axis=1)
-martmovies.drop('tagline', inplace=True, axis=1)
-martmovies.drop('budget_adj', inplace=True, axis=1)
-martmovies.drop('revenue_adj', inplace=True, axis=1)
-martmovies.drop('vote_count', inplace=True, axis=1)
-print(martmovies.head(3))
 
-martmovies.info()
-#3. Remove all rows pre 1975 (see 'Jaws' reference in document)
+
